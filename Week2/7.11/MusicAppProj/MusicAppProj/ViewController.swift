4536c04555dad01f68230bb9a9bc9a2b7b40376f
tekim <rldod526@naver.com>
2022-07-11 23:57:41 +0900

    Assignment & test Life Cycle

--- Week2/7.11/MusicAppProj/MusicAppProj/ViewController.swift
+++ Week2/7.11/MusicAppProj/MusicAppProj/ViewController.swift
@@ -12,11 +12,22 @@ class ViewController: UIViewController {
   
     
     @IBOutlet var imageList: [UIImageView]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad")
         makeUI()
+        print("viewDidLoad")
     }
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+
     func makeUI() {
         for image in imageList {
             image.layer.cornerRadius = 4.5
